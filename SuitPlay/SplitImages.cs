fe56918b32a1895c90d4bd7aface973006256d11
Yke Smit <yke.smit@gmail.com>
2026-04-25 16:48:34 +0200

    Disabled SkiaSharp to investigate the hanging build issue

--- SuitPlay/SplitImages.cs
+++ SuitPlay/SplitImages.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
-using SkiaSharp;
+//using SkiaSharp;
 
 namespace SuitPlay
 {
@@ -7,46 +7,47 @@ namespace SuitPlay
     {
         public static Dictionary<(string suit ,string card), string> Split(CardImageSettings imageSettings)
         {
-            var fileNames = ExtractAndSaveImages(imageSettings);
-            return CreateLookup(imageSettings, fileNames.ToArray());
+            //var fileNames = ExtractAndSaveImages(imageSettings);
+            //return CreateLookup(imageSettings, fileNames.ToArray());
+            return CreateLookup(imageSettings, [""]);
         }
 
-        private static IEnumerable<string> ExtractAndSaveImages(CardImageSettings imageSettings)
-        {
-            using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"SuitPlay.Resources.Images.Embedded.{imageSettings.CardImage}");
-            var bitmap = SKBitmap.Decode(stream);
-            var counter = 0;
+        //private static IEnumerable<string> ExtractAndSaveImages(CardImageSettings imageSettings)
+        //{
+        //    using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"SuitPlay.Resources.Images.Embedded.{imageSettings.CardImage}");
+        //    var bitmap = SKBitmap.Decode(stream);
+        //    var counter = 0;
 
-            for (int i = 0; i < 4; i++)
-                for (int j = 0; j < 13; j++)
-                {
-                    var newBitmap = new SKBitmap(imageSettings.CardWidth, imageSettings.CardHeight);
-                    var left = imageSettings.XOffSet + j * imageSettings.CardWidth;
-                    var top = imageSettings.YOffSet + i * imageSettings.CardHeight;
-                    var source = new SKRect(left, top, left + imageSettings.CardWidth - imageSettings.XCardPadding,
-                        top + imageSettings.CardHeight - imageSettings.YCardPadding);
-                    var dest = new SKRect(0, 0, imageSettings.CardWidth, imageSettings.CardHeight);
-                    // Copy 1/52 of the original into that bitmap
-                    using (var canvas = new SKCanvas(newBitmap))
-                    {
-                        canvas.DrawBitmap(bitmap, source, dest);
-                    }
+        //    for (int i = 0; i < 4; i++)
+        //        for (int j = 0; j < 13; j++)
+        //        {
+        //            var newBitmap = new SKBitmap(imageSettings.CardWidth, imageSettings.CardHeight);
+        //            var left = imageSettings.XOffSet + j * imageSettings.CardWidth;
+        //            var top = imageSettings.YOffSet + i * imageSettings.CardHeight;
+        //            var source = new SKRect(left, top, left + imageSettings.CardWidth - imageSettings.XCardPadding,
+        //                top + imageSettings.CardHeight - imageSettings.YCardPadding);
+        //            var dest = new SKRect(0, 0, imageSettings.CardWidth, imageSettings.CardHeight);
+        //            // Copy 1/52 of the original into that bitmap
+        //            using (var canvas = new SKCanvas(newBitmap))
+        //            {
+        //                canvas.DrawBitmap(bitmap, source, dest);
+        //            }
 
-                    yield return SaveBitmapToFile(newBitmap, counter, imageSettings);
-                    counter++;
-                }
+        //            yield return SaveBitmapToFile(newBitmap, counter, imageSettings);
+        //            counter++;
+        //        }
 
-            static string SaveBitmapToFile(SKBitmap bitmap, int counter, CardImageSettings imageSettings)
-            {
-                SKImage image = SKImage.FromBitmap(bitmap);
-                using SKData encodedData = image.Encode(SKEncodedImageFormat.Png, 100);
-                string imagePath = Path.Combine(FileSystem.CacheDirectory, $"{imageSettings.CardImage}-image-{counter}.png");
-                using var bitmapImageStream = File.Open(imagePath, FileMode.Create, FileAccess.Write, FileShare.None);
-                encodedData.SaveTo(bitmapImageStream);
+        //    static string SaveBitmapToFile(SKBitmap bitmap, int counter, CardImageSettings imageSettings)
+        //    {
+        //        SKImage image = SKImage.FromBitmap(bitmap);
+        //        using SKData encodedData = image.Encode(SKEncodedImageFormat.Png, 100);
+        //        string imagePath = Path.Combine(FileSystem.CacheDirectory, $"{imageSettings.CardImage}-image-{counter}.png");
+        //        using var bitmapImageStream = File.Open(imagePath, FileMode.Create, FileAccess.Write, FileShare.None);
+        //        encodedData.SaveTo(bitmapImageStream);
 
-                return imagePath;
-            }
-        }
+        //        return imagePath;
+        //    }
+        //}
 
 
         private static Dictionary<(string suit ,string card), string> CreateLookup(CardImageSettings cardImageSettings, IReadOnlyList<string> fileNames)
